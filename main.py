915765d1a2ab8f9141ee22561d9773a8c516bcce
pasi kanto <68000803+Pkanto@users.noreply.github.com>
2021-06-25 09:17:58 +0300

    html runko tehty

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post('/osake')
+
+def luo_osake():
+    return{
+        "koodi":"Luonti onnistui",
+        "viesti": "osake luotu"
+    }
