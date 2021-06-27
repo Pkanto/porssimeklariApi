8a9ef5ca2fcd3a2680e63abc17e766edcf0c1556
pasi kanto <68000803+Pkanto@users.noreply.github.com>
2021-06-27 09:21:39 +0300

    Sivun pohjan tekemistä

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates 
 
 app = FastAPI()
+templates = Jinja2Templates(directory = "templates")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def ilmoitustaulu(request: Request):
+    return templates.TemplateResponse("ilmoitustaulu.html", {
+        "request": request,
+        "somevar":2
+    })
 
 @app.post('/osake')
 
